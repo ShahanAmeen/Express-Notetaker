2b4b1044727e4f2fc2169c972b7e44f39bdc223d
Shahan Ameen <Shahanameen@gmail.com>
2023-10-30 17:43:59 -0500

    added adjustment process.env for heroku

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ const express = require('express');
 const path = require('path');
 //const api = require('./routes/index.js');
 let database = require("./db/db.json")
-const PORT = process.env.port || 3001;
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
