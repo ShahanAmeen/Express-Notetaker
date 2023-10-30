cbe2ab18a109251ac7f40203364203c6bfaf2b42
Shahan Ameen <Shahanameen@gmail.com>
2023-10-30 16:20:06 -0500

    first file

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+/*
+  1. A route that sends the home page
+  2. A route that sends the notes page
+
+  3. Make routes matching the fetch requests coming from the client-side code
+
+  db.json is the make-believe database
+
+  When a request comes in for the data:
+    - Read the file (fs.readFile)
+
+  When a request comes in to make a new note:
+    - Read the file 
+    - Add the new data 
+    - Write a new version of that file
+*/
+
